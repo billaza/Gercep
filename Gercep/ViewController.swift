1a76984c735acb7631b285ba1caf9d7b2b3c4e0c
billaza <nabillazarine@gmail.com>
2022-04-27 14:08:58 +0700

    Initial Commit

---
+++ Gercep/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gercep
+//
+//  Created by Nabilla Azarine on 27/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
